60bc18d7160f1d0bb973f91e07ef0fce6507021f
charathram <charath@crowdbotics.com>
2020-09-29 15:29:25 -0700

    Update screen

--- src/features/BlankScreen011417/screens/blank.js
+++ src/features/BlankScreen011417/screens/blank.js
@@ -16,7 +16,7 @@ export default class Blank extends React.Component {
 
   render = () => (
     <View style={styles.container}>
-      <Text>Welcome to Charath's New App 5</Text>
+      <Text>Welcome to Charath's New App 6</Text>
     </View>
   );
 }
