05c9ad21ae068ba7ab6745311fbd68d6f383e188
charathram <charath@crowdbotics.com>
2020-09-29 14:23:14 -0700

    Update screen

--- src/features/BlankScreen011417/screens/blank.js
+++ src/features/BlankScreen011417/screens/blank.js
@@ -16,7 +16,7 @@ export default class Blank extends React.Component {
 
   render = () => (
     <View style={styles.container}>
-      <Text>This is your new component</Text>
+      <Text>Welcome to Crowdbotics</Text>
     </View>
   );
 }
