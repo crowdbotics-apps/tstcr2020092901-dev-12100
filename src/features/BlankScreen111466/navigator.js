f7383898457ef5b2e2d6aa58f9c4d78481bec4fb
Crowdbotics <team@crowdbotics.com>
2020-10-05 18:14:17 +0000

    Update components

--- src/features/BlankScreen111466/navigator.js
+++ src/features/BlankScreen111466/navigator.js
@@ -1,6 +1,6 @@
 import { createStackNavigator } from "react-navigation-stack"
-import _Blank from "./screens/blank"
-export default _BlankNavigator = createStackNavigator(
-  { _Blank: { screen: _Blank } },
-  { initialRouteName: "_Blank" }
+import Blank from "./screens/blank"
+export default BlankNavigator = createStackNavigator(
+  { Blank: { screen: Blank } },
+  { initialRouteName: "Blank" }
 )
--- src/features/BlankScreen111466/screens/blank.js
+++ src/features/BlankScreen111466/screens/blank.js
@@ -12,99 +12,38 @@ import {
   Datepicker
 } from "react-native-ui-kitten"
 import Slider from "@react-native-community/slider"
-
 import { SlideMenuIcon } from "../../../navigator/slideMenuIcon"
 import { connect } from "react-redux"
-export class __Blank extends React.Component {
+export class _Blank extends React.Component {
   static navigationOptions = ({ navigation }) => {
     return { headerLeft: <SlideMenuIcon navigationProps={navigation} /> }
   }
   state = {}
   render = () => (
-    <View style={this.props.themedStyle.View_1}>
+    <View style={styles.View_1}>
       <Button
+        title="Press me!"
+        color="#3366FF"
         textStyle={{
-          fontSize: 14,
+          fontSize: 12,
           color: "#000000",
           textAlign: "center",
           fontWeight: "normal",
           fontStyle: "normal"
         }}
-        style={this.props.themedStyle.Button_3}
+        style={styles.Button_3}
         onPress={() => alert("Pressed!")}
-      >
-        Press me!
-      </Button>
+      />
     </View>
   )
 }
 
-_Blank = withStyles(__Blank, theme => ({
-  View_1: {
-    marginLeft: 0,
-    marginRight: 0,
-    marginTop: 0,
-    marginBottom: 0,
-    paddingLeft: 0,
-    paddingRight: 0,
-    paddingTop: 0,
-    paddingBottom: 0,
-    overflow: "visible",
-    fontSize: 12,
-    color: "#000000",
-    backgroundColor: "#ffffff",
-    fontStyle: "normal",
-    fontWeight: "normal",
-    flexDirection: "column",
-    flexWrap: "wrap",
-    justifyContent: "flex-start",
-    alignItems: "stretch",
-    alignContent: "stretch",
-    borderColor: "#000000",
-    borderStyle: "solid",
-    borderWidth: 0,
-    borderLeftWidth: 0,
-    borderRightWidth: 0,
-    borderTopWidth: 0,
-    borderBottomWidth: 0,
-    borderRadius: 0,
-    backgroundSize: "auto"
-  },
-  Button_3: {
-    width: "100%",
-    marginLeft: 0,
-    marginRight: 0,
-    marginTop: 5,
-    marginBottom: 5,
-    paddingLeft: 5,
-    paddingRight: 5,
-    paddingTop: 5,
-    paddingBottom: 5,
-    overflow: "visible",
-    fontSize: 12,
-    color: "#000000",
-    backgroundColor: "#3366FF",
-    fontStyle: "normal",
-    fontWeight: "normal",
-    borderColor: "#000000",
-    borderStyle: "solid",
-    borderWidth: 0,
-    borderLeftWidth: 0,
-    borderRightWidth: 0,
-    borderTopWidth: 0,
-    borderBottomWidth: 0,
-    borderRadius: 0,
-    textAlign: "center",
-    textAlignVertical: "center",
-    textDecorationLine: "none",
-    textTransform: "none",
-    lineHeight: 12,
-    letterSpacing: 0
-  }
+Blank = withStyles(_Blank, theme => ({
+  container: { backgroundColor: theme["color-basic-100"] }
 }))
 
 function mapStateToProps(state) {
-  return { state: state }
+  return { state: state.apiReducer }
 }
 const actionCreators = {}
-export default connect(mapStateToProps, actionCreators)(_Blank)
+export default connect(mapStateToProps, actionCreators)(Blank)
